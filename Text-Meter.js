5f15fbfa927a1d2575286d2a1ee73a23e7a274af
Vital Vuillaume <vitalvuillaume@proton.me>
2023-07-28 14:48:35 +0200

    Add files via upload

---
+++ Text-Meter.js
@@ -0,0 +1,18 @@
+const txt = document.querySelector(".txt");
+const nb = document.querySelector(".nb");
+
+//------Caractere qui augmente------\\
+
+txt.addEventListener("input", function() {
+    const texte = txt.value;
+    const nbCaracteres = texte.length;
+    nb.textContent = nbCaracteres;
+});
+
+//------Nombre de caractere dans le textarea------\\
+
+function limitNumberLength(input, maxLength) {
+    if (input.value.length > maxLength) {
+      input.value = input.value.slice(0, maxLength);
+    }
+  }
